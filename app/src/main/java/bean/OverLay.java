58cf74132a3cf2a702eefd81afa616743d509611
nick <1831003848@qq.com>
2018-04-16 11:50:27 +0800

    实现地图自定义风格

---
+++ app/src/main/java/bean/OverLay.java
@@ -0,0 +1,78 @@
+package bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by 孵孵 on 2018/4/15 0015.
+ */
+
+public class OverLay implements Serializable{
+
+    double lat,lng;
+    String name,type;
+
+    public OverLay(double lat, double lng, String name, String type) {
+        this.lat = lat;
+        this.lng = lng;
+        this.name = name;
+        this.type = type;
+    }
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OverLay overLay = (OverLay) o;
+
+        if (Double.compare(overLay.lat, lat) != 0) return false;
+        if (Double.compare(overLay.lng, lng) != 0) return false;
+        if (name != null ? !name.equals(overLay.name) : overLay.name != null) return false;
+        return type != null ? type.equals(overLay.type) : overLay.type == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(lat);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(lng);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+}
--- app/src/main/java/com/ff/wei/MapActivity.java
+++ app/src/main/java/com/ff/wei/MapActivity.java
@@ -1,13 +1,395 @@
 package com.ff.wei;
 
+import bean.OverLay;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapPoi;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static android.util.Log.d;
 
 public class MapActivity extends AppCompatActivity {
+    private BaiduMap.OnMarkerClickListener markListener=null;
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+    //模式切换，正常模式
+    private boolean modeFlag = true;
+    //当前地图缩放级别
+    private float zoomLevel;
+    //定位相关
+    private LocationClient mLocationClient;
+    private MyLocationListener mLocationListener;
+    //是否第一次定位，如果是第一次定位的话要将自己的位置显示在地图 中间
+    private boolean isFirstLocation = true;
+    //创建自己的箭头定位
+    private BitmapDescriptor bitmapDescriptor;
+    //经纬度
+    double mLatitude;
+    double mLongitude;
+    //方向传感器监听
+    private float mLastX;
+    //显示marker
+    private boolean showMarker = false;
 
+    private List<OverLay> overLays=new ArrayList<>();
+    private Button addOver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        makePermission();// 请求权限
+        setMapCustomFile();
+
+
+
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_map);
+
+        //初始化控件
+        initView();
+        //初始化地图
+        initMap();
+        initLocation();
+        //创建自己的定位图标，结合方向传感器，定位的时候显示自己的方向
+        //initMyLoc();
+        //创建marker信息
+        d("status1","finished Created");
     }
+
+    private void initMyLoc() {
+        //初始化图标
+        bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher_round);
+    }
+    private void initMap() {
+        //获取地图控件引用
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        // 不显示缩放比例尺
+        mMapView.showZoomControls(false);
+        // 不显示百度地图Logo
+        mMapView.removeViewAt(1);
+        //百度地图
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setOnMapLoadedCallback(new BaiduMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                MapView.setMapCustomEnable(true);
+            }
+        });
+
+        // 改变地图状态
+        MapStatus mMapStatus = new MapStatus.Builder().zoom(15).build();
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
+        //设置地图状态改变监听器
+        mBaiduMap.setOnMapStatusChangeListener(new BaiduMap.OnMapStatusChangeListener() {
+
+
+            @Override
+            public void onMapStatusChangeStart(MapStatus mapStatus) {
+
+            }
+
+            @Override
+            public void onMapStatusChangeStart(MapStatus mapStatus, int i) {
+
+            }
+
+            @Override
+            public void onMapStatusChangeFinish(MapStatus arg0) {
+            }
+            @Override
+            public void onMapStatusChange(MapStatus arg0) {
+                //当地图状态改变的时候，获取放大级别
+                zoomLevel = arg0.zoom;
+            }
+        });
+        //地图点击事件
+        mBaiduMap.setOnMapClickListener(new BaiduMap.OnMapClickListener() {
+            @Override
+            public boolean onMapPoiClick(MapPoi arg0) {
+                return false;
+            }
+            @Override
+            public void onMapClick(LatLng arg0) {
+            }
+        });
+    }
+    private void initLocation() {
+        //定位客户端的设置
+        mLocationClient = new LocationClient(this);
+        mLocationListener = new MyLocationListener();
+        //注册监听
+        mLocationClient.registerLocationListener(mLocationListener);
+        //配置定位
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setCoorType("bd09ll");//坐标类型
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//打开Gps
+        option.setScanSpan(1000);//毫秒定位一次
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        mLocationClient.setLocOption(option);
+
+    }
+    private void initView() {
+        //地图控制按钮
+        addOver=(Button)findViewById(R.id.addOverLay);
+
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addOverLay:
+                addOverlayTest();
+                break;
+        }
+    }
+    //显示marker
+    private void addOverlay(OverLay over) {
+        //清空地图
+        mBaiduMap.clear();
+        //创建marker的显示图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher);
+        LatLng latLng = null;
+        Marker marker;
+        OverlayOptions options;
+            latLng = new LatLng(over.getLat(),over.getLng());
+            //设置marker
+            options = new MarkerOptions()
+                    .position(latLng)//设置位置
+                    .icon(bitmap)//设置图标样式
+                    .zIndex(9) // 设置marker所在层级
+                    .draggable(false); // 设置手势拖拽;
+            //添加marker
+            marker = (Marker) mBaiduMap.addOverlay(options);
+            //使用marker携带info信息，当点击事件的时候可以通过marker获得info信息
+            Bundle bundle = new Bundle();
+            //info必须实现序列化接口
+            bundle.putSerializable("overLays", over);
+            marker.setExtraInfo(bundle);
+        //添加marker点击事件的监听
+        if(markListener!=null){
+            mBaiduMap.removeMarkerClickListener(markListener); //防止监听重叠
+        }
+        markListener= new BaiduMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                //从marker中获取info信息
+                Bundle bundle = marker.getExtraInfo();
+                OverLay u = (OverLay) bundle.getSerializable("overLays");
+                //将信息显示在界面上
+                //Log.d("info",infoUtil.toString());
+                if(u==null){
+                    d("info","no getting");
+                    return false;
+                }
+                d("info","clickTest:"+u.getLat());
+                return true;
+            }
+        };
+        mBaiduMap.setOnMarkerClickListener(markListener);
+    }
+    //自定义的定位监听
+    class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //将获取的location信息给百度map
+
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(mLastX)
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build();
+            mBaiduMap.setMyLocationData(data);
+            //更新经纬度
+            mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+            Log.d("MyLat:",mLatitude+"");
+            //配置定位图层显示方式，使用自己的定位图标
+            MyLocationConfiguration configuration = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, true, bitmapDescriptor);
+            mBaiduMap.setMyLocationConfigeration(configuration);
+            if(isFirstLocation&&location.getAddrStr()!=null){
+                showInfo("loc:"+location+"\nlat:"+location.getLatitude()+"\nlng:"+location.getLongitude());
+                showInfo("位置：" + location.getAddrStr());
+                Log.d("testt","loc:"+location+"\nlat:"+location.getLatitude()+"\nlng:"+location.getLongitude());
+                //获取经纬度
+                LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());
+                OverLay myself=new OverLay(location.getLatitude(),location.getLongitude(),"nick","user");
+                addOverlay(myself);
+                MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(ll);
+                mBaiduMap.setMapStatus(status);//直接到中间
+               // mBaiduMap.animateMapStatus(status);//动画的方式到中间
+                isFirstLocation = false;
+            }
+            //mBaiduMap.clear();
+            if(!overLays.contains(new OverLay(mLatitude,mLongitude,"test","user"))){
+                overLays.add(new OverLay(mLatitude,mLongitude,"test","user") );
+            }
+            //addOverlay(overLays);
+
+        }
+    }
+  public void makePermission(){
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MapActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String[] permissionStr = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MapActivity.this, permissionStr, 111);//requestCode=1
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 111:                                                             //requestCode=1
+                if (grantResults.length > 0) {
+                    for (int x = 0; x < grantResults.length && x < permissions.length; ++x) {
+                        if (grantResults[x] != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(MapActivity.this, "同意啊", Toast.LENGTH_SHORT).show();
+                            ActivityCompat.requestPermissions(MapActivity.this, new String[]{permissions[x]}, 1);   //requestCode=1
+                        }
+                    }
+                } else {
+                    Toast.makeText(MapActivity.this, "未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        mBaiduMap.setMyLocationEnabled(true);
+        if(!mLocationClient.isStarted()){
+            mLocationClient.start();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //关闭定位
+        mBaiduMap.setMyLocationEnabled(false);
+        if(mLocationClient.isStarted()){
+            mLocationClient.stop();
+        }
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    //显示消息
+    private void showInfo(String str){
+        Toast.makeText(MapActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void setMapCustomFile() {
+        FileOutputStream out = null;
+        InputStream inputStream = null;
+        try {
+            Log.d("Status","ok");
+            inputStream = getAssets().open("custom_configdir.txt");
+
+            byte[] b = new byte[inputStream.available()];
+            inputStream.read(b);
+            String moduleName ="/storage/emulated/0/";
+            File f = new File(moduleName +"/map_style.txt");
+            if (f.exists()) {
+                f.delete();
+            }
+            f.createNewFile();
+            out = new FileOutputStream(f);
+            out.write(b);
+            MapView.setCustomMapStylePath(moduleName + "/map_style.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void addOverlayTest(){
+
+    }
+
 }
