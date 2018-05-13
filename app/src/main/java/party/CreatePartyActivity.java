e552cab73cc4f50fb052f9a2f8163926416944f3
nickdsb <1831003848@qq.com>
2018-05-13 15:33:54 +0800

    新增活动详情布局

--- app/src/main/java/party/CreatePartyActivity.java
+++ app/src/main/java/party/CreatePartyActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.ff.wei.R;
@@ -18,12 +19,16 @@ public class CreatePartyActivity extends Activity implements View.OnClickListene
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.create_party);
         TextView midTitle= (TextView) findViewById(R.id.title_MID);
+        Button back= (Button) findViewById(R.id.title_L);
+        back.setOnClickListener(this);
         midTitle.setText("创建活动");
-
     }
-
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.title_L:
+                finish();
+                break;
+        }
     }
 }
