bc0a2ab3ebeb1caf8a83e7823d913660858d1730
wbly <wbly.kr@gmail.com>
2018-05-16 00:04:04 +0800

    实现消息列表和聊天界面

---
+++ app/src/main/java/com/ff/message/MessageActivity.java
@@ -0,0 +1,79 @@
+package com.ff.message;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.ff.Bean.Message;
+import com.ff.wei.R;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageActivity extends AppCompatActivity {
+
+    private ActionBar actionBar;
+    private EditText editText = (EditText)findViewById(R.id.inputbox);
+    private Button sendButton = (Button) findViewById(R.id.sendButton);
+    private RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        Intent intent = getIntent();
+        Message chosenMessage = (Message) intent.getSerializableExtra("chosenMessage");
+
+//        if(chosenMessage==null)
+//            return;
+//        actionBar = getSupportActionBar();
+//        if(chosenMessage.getGroup()==null&&chosenMessage.getTo()!=null)
+//            actionBar.setTitle(chosenMessage.getTo());
+//        else if(chosenMessage.getGroup()!=null&&chosenMessage.getTo()==null)
+//            actionBar.setTitle(chosenMessage.getGroup());
+//        actionBar.show();
+
+
+
+        //********向服务器查数据************
+        //提交时间段和用户ID、gruopID
+
+
+        //**********本地测试数据（私聊）****************
+        Message[] PriavateTemp = new Message[12];
+        List<Message> priavateList = new ArrayList<>();
+        for(int i=0 ;i<12;i+=4)
+        {
+            priavateList.add(new Message(new Timestamp(System.currentTimeMillis()),null,"me","UserA","conentcontent1",null));
+            priavateList.add(new Message(new Timestamp(System.currentTimeMillis()),null,"me","UserA","conentcontent2",null));
+            priavateList.add(new Message(new Timestamp(System.currentTimeMillis()),null,"UserA",null,"conentcontent3",null));
+            priavateList.add(new Message(new Timestamp(System.currentTimeMillis()),null,"me","UserA","conentcontent4",null));
+        }
+        //***************************
+
+        //**********本地测试数据（群聊）****************
+        Message[] GroupTemp = new Message[12];
+        List<Message> GroupList =new ArrayList<>();
+        for(int i=0 ;i<12;i+=4)
+        {
+            GroupList.add(new Message(new Timestamp(System.currentTimeMillis()),"GroupA","me",null,"conentcontent1",null));
+            GroupList.add(new Message(new Timestamp(System.currentTimeMillis()),"GroupA","UserA",null,"conentcontent2",null));
+            GroupList.add(new Message(new Timestamp(System.currentTimeMillis()),"GroupA","UserB",null,"conentcontent3",null));
+            GroupList.add(new Message(new Timestamp(System.currentTimeMillis()),"GroupA","UserA",null,"conentcontent4",null));
+        }
+        //***************************
+
+        rv.setAdapter(new ChatAdapter(GroupList));
+    }
+
+
+}
