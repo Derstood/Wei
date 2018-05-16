2e05a1dc716dd09b8acf5d6413aecc528333a19d
wbly <wbly.kr@gmail.com>
2018-05-16 14:47:52 +0800

    新的！！实现消息列表和聊天界面

---
+++ app/src/main/java/com/ff/Bean/GroupMessage.java
@@ -0,0 +1,9 @@
+package com.ff.Bean;
+
+/**
+ * Created by Kr on 2018/5/9.
+ */
+
+public class GroupMessage extends Message {
+
+}
--- app/src/main/java/com/ff/Bean/Message.java
+++ app/src/main/java/com/ff/Bean/Message.java
@@ -1,28 +1,66 @@
 package com.ff.Bean;
 
+import java.io.Serializable;
+import java.sql.Timestamp;
+
 /**
  * Created by Kr on 2018/4/30.
  */
 
-public class Message {
-    private int type; //1表示群聊消息，2表示私聊消息
-    private String name;
+public class Message implements Serializable{
+
+    private Timestamp time;
+    private String group;      //私聊消息的group为空，群聊消息的to为空
+    private String from;        //group,from,to都是ID
+    private String to;
     private String content;
+    private String avatarURL;
 
-    public int getType() {
-        return type;
+
+
+    public Message() {
     }
 
-    public void setType(int type) {
-        this.type = type;
+
+    public Message(Timestamp time, String group , String from, String to, String content,String avatarURL) {
+        this.time = time;
+        this.group = group;
+        this.from = from;
+        this.to = to;
+        this.content = content;
+        this.avatarURL = avatarURL;
     }
 
-    public String getName() {
-        return name;
+    public Timestamp getTime() {
+        return time;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
     }
 
     public String getContent() {
@@ -33,12 +71,19 @@ public class Message {
         this.content = content;
     }
 
+    public String getAvatarURL() { return avatarURL; }
+
+    public void setAvatarURL(String avatarURL) { this.avatarURL = avatarURL; }
+
     @Override
     public String toString() {
         return "Message{" +
-                "type=" + type +
-                ", name='" + name + '\'' +
+                "time=" + time +
+                ", group='" + group + '\'' +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
                 ", content='" + content + '\'' +
+                ", avatarURL='" + avatarURL + '\'' +
                 '}';
     }
 }
